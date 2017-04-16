74e2dbaceeaa08c2aace30c079800952674b115d
Anton Outkine <antonoutkine@gmail.com>
2017-04-15 19:16:26 -0700

    v4 permutation - better GUI, program split into 2 files, castling, pawn promotion, and en passant support

--- chess_gui.py
+++ chess_gui.py
@@ -3,24 +3,31 @@ from spritesheet import BlockSheet
 from time import time
 
 from main import play as play1
-# from main2 import play as play2
+from main import play as play2
 
+from main import PIECE_ATTACK_FUNCTIONS
+from main import KING_CASTLE_POSITIONS
 from main import BOARD_ITERATOR
 from main import EMPTY
 from main import PIECE_MOVE_FUNCTIONS
 from main import find_state
 from main import find_type
-from main import apply_move
+from main import process_move
+from main import update_constants
+from main import update_double_pawn
+from main import find_castle_directions
+from main import castle
+from main import pawn_move
 
 # @formatter:off
 DEFAULT_BOARD = (
     # 0         1           2           3           4           5           6           7
-    ((1, 'r'), 	(1, 'n'), 	(1, 'b'), 	(1, 'q'), 	(1, 'k'), 	(1, 'b'), 	(1, 'n'), 	(1, 'r'), 	),  # 0
-    ((1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	),  # 1
-    ((0,),  	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	),  # 2
-    ((0,),  	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	),  # 3
-    ((0,),  	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	),  # 4
-    ((0,),  	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	(0,),	 	),  # 5
+    ((1, 'r'), 	(1, 'n'), 	(1, 'b'), 	(1, 'q'), 	(1, 'k'), 	(1, 'b'), 	(1, 'n'), 	(1, 'r'),   ),  # 0
+    ((1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'), 	(1, 'p'),   ),  # 1
+    ((0, 0),  	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),     ),  # 2
+    ((0, 0),  	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	),  # 3
+    ((0, 0),  	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	),  # 4
+    ((0, 0),  	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	(0, 0),	 	),  # 5
     ((-1, 'p'), (-1, 'p'), 	(-1, 'p'), 	(-1, 'p'), 	(-1, 'p'), 	(-1, 'p'), 	(-1, 'p'), 	(-1, 'p'), 	),  # 6
     ((-1, 'r'), (-1, 'n'), 	(-1, 'b'), 	(-1, 'q'), 	(-1, 'k'), 	(-1, 'b'), 	(-1, 'n'), 	(-1, 'r'), 	),  # 7
 )
@@ -39,8 +46,9 @@ NUMBER_GAP = 25
 SHOW_NUMBERS = False
 PLAYER_TILE_COLOR = (174, 126, 126)
 
-COMPUTER_SIDES = (1,)
-COMPUTERS = (None, play1, None)
+COMPUTER_SIDES = (1, -1)
+# COMPUTER_SIDES = ()
+COMPUTERS = (None, play1, play2)
 CONFIRM_TURN = False
 
 DISPLAY = pygame.display.set_mode([TILE_SIZE * 8 for _ in range(2)])
@@ -72,18 +80,18 @@ def draw_board(display, piece_sprites, number_sprites, board, piece_tile):
                 display.blit(number_sprites[y], (coordinates[0] + NUMBER_GAP, coordinates[1]))
 
             if tile_state != EMPTY:
+                # print(x, y,  board[y][x])
                 sprite = piece_sprites[tile_state][find_type((x, y), board)]
                 display.blit(sprite, find_center(TILE_DIMENSIONS, sprite.get_size(), coordinates))
 
-times = [None, None, None]
-
-def run(board, side, turn):
+def run(board, side, turn, double_pawn):
     current_time = time()
-    board = COMPUTERS[side](board, side, side)
-    times[side] = time() - current_time
+    board, double_pawn = COMPUTERS[side](board, side, double_pawn)
+    # print(turn, side, time() - current_time)
+    print(time() - current_time)
     # print("##############################################")
     turn, side = update(turn, side)
-    return board, side, turn
+    return board, side, turn, double_pawn
 
 def update(turn, side):
     return turn + 1, side * -1
@@ -95,10 +103,11 @@ def main():
     board = list(DEFAULT_BOARD)
     side = -1
     turn = 1
-    # for _ in range(50):
-    #     board, side, turn = run(board, side, turn)
     piece = None
+    double_pawn = None
     while True:
+        draw_board(DISPLAY, PIECE_SPRITES, NUMBER_SPRITES, board, piece)
+        pygame.display.update()
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -106,7 +115,7 @@ def main():
 
         if side in COMPUTER_SIDES:
             if not CONFIRM_TURN or pygame.key.get_pressed()[pygame.K_SPACE]:
-                board, side, turn = run(board, side, turn)
+                board, side, turn, double_pawn = run(board, side, turn, double_pawn)
 
         else:
             for event in events:
@@ -117,13 +126,33 @@ def main():
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if piece:
                         move = tuple(convert_to_grid(event.pos))
-                        if move in PIECE_MOVE_FUNCTIONS[find_type(piece, board)](piece, board, side):
-                            board = apply_move(move, piece, board, side)
+                        piece_type = find_type(piece, board)
+                        if piece_type == 'p':
+                            moves, passant_moves = pawn_move(piece, board, side, double_pawn)
+                        else:
+                            moves = PIECE_MOVE_FUNCTIONS[piece_type](piece, board, side)
+                            passant_moves = []
+                        if move in moves:
+                            update_constants(board, side, piece)
+                            board = process_move(board, piece, move, side, move in passant_moves)
+                            double_pawn = update_double_pawn(piece, move, piece_type)
                             turn, side = update(turn, side)
+                        elif piece_type == 'k' and move in KING_CASTLE_POSITIONS[side]:
+                            enemy_attacks = {}
+                            for y in BOARD_ITERATOR:
+                                for x in BOARD_ITERATOR:
+                                    if find_state((x, y), board) == side * -1:
+                                        attacks = PIECE_ATTACK_FUNCTIONS[piece_type]((x, y), board, side * -1)
+                                        if attacks:
+                                            for attack in attacks:
+                                                enemy_attacks.setdefault(attack, []).append((x, y))
+                            castle_directions = find_castle_directions(board, side, enemy_attacks)
+                            for direction in castle_directions:
+                                if KING_CASTLE_POSITIONS[side][direction] == move:
+                                    board = castle(board, side, direction)
+                                    turn, side = update(turn, side)
+                                    break
                         piece = None
-
-        draw_board(DISPLAY, PIECE_SPRITES, NUMBER_SPRITES, board, piece)
-        pygame.display.update()
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from math import sqrt
-from copy import deepcopy
 
 BOARD_SIZE = 8
 BOARD_ITERATOR = range(BOARD_SIZE)
 EMPTY = 0
+EMPTY_TILE = (EMPTY, EMPTY)
 BORDER = 2
 KNIGHT_VECTORS = ((-1, -2), (1, -2),
                   (2, -1), (2, 1),
@@ -20,16 +20,36 @@ BROAD_CENTER = {(2, 2), (3, 2), (4, 2), (5, 2),
                 (2, 5), (3, 5), (4, 5), (5, 5)}
 CENTER = {(3, 3), (3, 4), (4, 3), (4, 4)}
 PAWN_STARTS = (None, 1, 6)
+BOARD_EDGES = (0, 7)
 
 ACQUIRE_BONUS = 5
 BROAD_CENTER_BONUS = 1
-CENTER_BONUS = 2
+CENTER_BONUS = 4
 LIGHT_OPENING_BONUS = 1
 KING_BONUS = 2
+PAWN_BONUS = 1
+# MAX_SIMULATION_LEVELS = ((17, 4), (10, 5), (8, 6), (5, 7), (1, 8))
 MAX_SIMULATION_LEVEL = 4
-
 PIECE_VALUES = {'p': 1, 'n': 3, 'b': 3.25, 'r': 5, 'q': 9, 'k': 20}
 PIECE_POINTS = {key: PIECE_VALUES[key] * ACQUIRE_BONUS for key in PIECE_VALUES}
+PAWN_PROMOTION_POINTS = PIECE_POINTS['q'] - PIECE_POINTS['p']
+
+UNMOVED_KING = [None, True, True]
+UNMOVED_ROOKS = [None, {(0, 0): True, (7, 0): True}, {(0, 7): True, (7, 7): True}]
+ROOK_POSITIONS = (None, ((0, 0), (7, 0)), ((0, 7), (7, 7)))
+KING_POSITIONS = (None, (4, 0), (4, 7))
+ROOK_CASTLE_POSITIONS = (None, ((3, 0), (5, 0)), ((3, 7), (5, 7)))
+KING_CASTLE_POSITIONS = (None, ((2, 0), (6, 0)), ((2, 7), (6, 7)))
+CASTLE_TILES = (
+    None,
+    (((2, 0), (3, 0)), ((6, 0), (5, 0))),
+    (((2, 7), (3, 7)), ((6, 7), (5, 7)))
+)
+KING_PATH = (
+    None,
+    (((1, 0), (2, 0), (3, 0)), ((6, 0), (5, 0))),
+    (((1, 7), (2, 7), (3, 7)), ((6, 7), (5, 7)))
+)
 
 def distance(pos1, pos2):
     return sqrt((pos2[1] - pos1[1]) ** 2 + (pos2[0] - pos1[0]) ** 2)
@@ -50,12 +70,16 @@ def safe_find_state(pos, board):
         return board[pos[1]][pos[0]][0]
     return BORDER
 
-def pawn_move(loc, board, side):
+def pawn_move(loc, board, side, double_pawn):
     moves = []
+    passant_moves = []
     for x in (-1, 1):
         pos = (x + loc[0], side + loc[1])
         if safe_find_state(pos, board) == side * -1:
             moves.append(pos)
+        elif (loc[0] + x, loc[1]) == double_pawn:
+            moves.append(pos)
+            passant_moves.append(pos)
     pos = (loc[0], loc[1] + side)
     if safe_find_state(pos, board) == EMPTY:
         moves.append(pos)
@@ -63,10 +87,7 @@ def pawn_move(loc, board, side):
             pos = (loc[0], loc[1] + 2 * side)
             if find_state(pos, board) == EMPTY:
                 moves.append(pos)
-    return moves
-
-def pawn_attack(loc, board, side):
-    return [(x + loc[0], side + loc[1]) for x in (-1, 1)]
+    return moves, passant_moves
 
 def jump_piece_move(loc, board, side, vectors):
     moves = []
@@ -81,20 +102,6 @@ def knight_move(loc, board, side):
 
 def king_move(loc, board, side):
     return jump_piece_move(loc, board, side, KING_VECTORS)
-
-def jump_piece_attack(loc, vectors):
-    attacks = []
-    for vector in vectors:
-        pos = (loc[0] + vector[0], loc[1] + vector[1])
-        if within_bounds(pos):
-            attacks.append(pos)
-    return attacks
-
-def knight_attack(loc, board, side):
-    return jump_piece_attack(loc, KNIGHT_VECTORS)
-
-def king_attack(loc, board, side):
-    return jump_piece_attack(loc, KING_VECTORS)
 
 def vector_piece_move(loc, board, side, vectors):
     moves = []
@@ -119,6 +126,26 @@ def rook_move(loc, board, side):
 def queen_move(loc, board, side):
     return bishop_move(loc, board, side) + rook_move(loc, board, side)
 
+# noinspection PyUnusedLocal
+def pawn_attack(loc, board, side):
+    return [(x + loc[0], side + loc[1]) for x in (-1, 1)]
+
+def jump_piece_attack(loc, vectors):
+    attacks = []
+    for vector in vectors:
+        pos = (loc[0] + vector[0], loc[1] + vector[1])
+        if within_bounds(pos):
+            attacks.append(pos)
+    return attacks
+
+# noinspection PyUnusedLocal
+def knight_attack(loc, board, side):
+    return jump_piece_attack(loc, KNIGHT_VECTORS)
+
+# noinspection PyUnusedLocal
+def king_attack(loc, board, side):
+    return jump_piece_attack(loc, KING_VECTORS)
+
 def vector_piece_attack(loc, board, vectors):
     attacks = []
     for vector in vectors:
@@ -133,19 +160,92 @@ def vector_piece_attack(loc, board, vectors):
                 break
     return attacks
 
+# noinspection PyUnusedLocal
 def bishop_attack(loc, board, side):
     return vector_piece_attack(loc, board, BISHOP_VECTORS)
 
+# noinspection PyUnusedLocal
 def rook_attack(loc, board, side):
     return vector_piece_attack(loc, board, ROOK_VECTORS)
 
+# noinspection PyUnusedLocal
 def queen_attack(loc, board, side):
     return bishop_attack(loc, board, side) + rook_attack(loc, board, side)
 
-PIECE_MOVE_FUNCTIONS = {'p': pawn_move, 'n': knight_move, 'b': bishop_move, 'r': rook_move, 'q': queen_move,
-                        'k': king_move}
+PIECE_MOVE_FUNCTIONS = {'n': knight_move, 'b': bishop_move, 'r': rook_move, 'q': queen_move, 'k': king_move}
 PIECE_ATTACK_FUNCTIONS = {'p': pawn_attack, 'n': knight_attack, 'b': bishop_attack, 'r': rook_attack, 'q': queen_attack,
                           'k': king_attack}
+
+def is_double_pawn(origin, move, piece_type):
+    if piece_type == 'p' and abs(move[1] - origin[1]) == 2:
+        return True
+    return False
+
+def allow_modify(func):
+    def temp(board, *args):
+        board = list(board)
+        func(board, *args)
+        return tuple(board)
+
+    return temp
+
+def modify_tile(board, tile, new_tile):
+    board[tile[1]] = list(board[tile[1]])
+    board[tile[1]][tile[0]] = new_tile
+    board[tile[1]] = tuple(board[tile[1]])
+
+@allow_modify
+def safe_modify_tile(board, tile, new_tile):
+    modify_tile(board, tile, new_tile)
+
+def apply_move(board, origin, move, side):
+    modify_tile(board, move, (side, find_type(origin, board)))
+    modify_tile(board, origin, EMPTY_TILE)
+
+@allow_modify
+def safe_apply_move(board, origin, move, side):
+    apply_move(board, origin, move, side)
+
+@allow_modify
+def castle(board, side, direction):
+    UNMOVED_KING[side] = False
+    apply_move(board, ROOK_POSITIONS[side][direction], ROOK_CASTLE_POSITIONS[side][direction], side)
+    apply_move(board, KING_POSITIONS[side], KING_CASTLE_POSITIONS[side][direction], side)
+
+@allow_modify
+def process_move(board, origin, move, side, passant_move):
+    apply_move(board, origin, move, side)
+    if can_promote(find_type(move, board), move):
+        modify_tile(board, move, (side, 'q'))
+    if passant_move:
+        modify_tile(board, (move[0], move[1] - side), EMPTY_TILE)
+
+
+def find_castle_directions(board, side, enemy_attacks):
+    directions = []
+    if UNMOVED_KING[side] and (True in UNMOVED_ROOKS[side].values()) and KING_POSITIONS[side] not in enemy_attacks:
+        for direction in (0, 1):
+            coordinates = ROOK_POSITIONS[side][direction]
+            if UNMOVED_ROOKS[side][coordinates] and find_state(coordinates, board) == side:
+                for coordinates in CASTLE_TILES[side][direction]:
+                    if find_state(coordinates, board) != EMPTY or coordinates in enemy_attacks:
+                        break
+                else:
+                    directions.append(direction)
+    return directions
+
+def can_promote(piece_type, move):
+    if piece_type == 'p' and move[1] in BOARD_EDGES:
+        return True
+    return False
+
+def analyze_board(board, side):
+    scores = [None, 0, 0]
+    for y in BOARD_ITERATOR:
+        for x in BOARD_ITERATOR:
+            if find_state((x, y), board) != EMPTY:
+                scores[find_state((x, y), board)] += PIECE_POINTS[find_type((x, y), board)]
+    return scores[side] - scores[side * -1]
 
 def analyze_movement(move, origin, piece_type, enemy_king, distance_to_king):
     points = 0
@@ -154,17 +254,24 @@ def analyze_movement(move, origin, piece_type, enemy_king, distance_to_king):
             points += CENTER_BONUS
         elif origin not in BROAD_CENTER and move in BROAD_CENTER:
             points += BROAD_CENTER_BONUS
-    new_distance = distance(move, enemy_king)
-    if new_distance < distance_to_king:
-        points += KING_BONUS
+        elif piece_type == 'p':
+            points += PAWN_BONUS
+    if piece_type not in ('k', 'p'):
+        new_distance = distance(move, enemy_king)
+        if new_distance < distance_to_king:
+            points += KING_BONUS
     return points
 
-def analyze_exchanges(move, origin, piece_type, board, ally_attacks, enemy_attacks, piece_points):
-    points = 0
+def analyze_exchanges(move, origin, piece_type, piece_points, board, ally_attacks, enemy_attacks, passant_moves):
     exchange_points = 0
+    if can_promote(piece_type, move):
+        piece_points = PIECE_POINTS['q']
+        exchange_points += PAWN_PROMOTION_POINTS
     if find_state(move, board) != EMPTY:
-        exchange_points = PIECE_POINTS[find_type(move, board)]
-        points += exchange_points
+        exchange_points += PIECE_POINTS[find_type(move, board)]
+    elif move in passant_moves:
+        exchange_points += PIECE_POINTS['p']
+    points = exchange_points
 
     enemy_number = len(enemy_attacks.get(move, []))
     ally_number = len(ally_attacks.get(move, []))
@@ -176,37 +283,36 @@ def analyze_exchanges(move, origin, piece_type, board, ally_attacks, enemy_attac
             points -= piece_points
     return exchange_points, points
 
-def apply_move(move, origin, board, side):
-    temp_board = list(board)
-    temp_board[move[1]] = list(temp_board[move[1]])
-    temp_board[move[1]][move[0]] = (side, find_type(origin, board))
-    temp_board[move[1]] = tuple(temp_board[move[1]])
-    temp_board[origin[1]] = list(temp_board[origin[1]])
-    temp_board[origin[1]][origin[0]] = (EMPTY,)
-    temp_board[origin[1]] = tuple(temp_board[origin[1]])
-    return tuple(temp_board)
+def update_constants(board, side, piece):
+    piece_type = find_type(piece, board)
+    # noinspection PyTypeChecker
+    if piece_type == 'k' and UNMOVED_KING[side]:
+        UNMOVED_KING[side] = False
+    elif piece_type == 'r' and UNMOVED_ROOKS[side].get(piece, False):
+        UNMOVED_ROOKS[side][piece] = False
 
-def analyze_board(board, side):
-    # TODO test speed of dict vs list
-    scores = [None, 0, 0]
-    for y in BOARD_ITERATOR:
-        for x in BOARD_ITERATOR:
-            if find_state((x, y), board) != EMPTY:
-                scores[find_state((x, y), board)] += PIECE_POINTS[find_type((x, y), board)]
-    return scores[side] - scores[side * -1]
+def update_double_pawn(piece, move, piece_type):
+    if is_double_pawn(piece, move, piece_type):
+        return move
+    return None
 
-def play(board, base_side, side, current_points=None, simulation_level=1):
+def play(board, side, double_pawn, current_points=None, simulation_level=1):
     moves = {}
     ally_attacks = {}
     enemy_attacks = {}
     enemy_king = None
+    passant_moves = []
     for y in BOARD_ITERATOR:
         for x in BOARD_ITERATOR:
             tile_state = find_state((x, y), board)
             if tile_state != EMPTY:
                 piece_type = find_type((x, y), board)
                 if tile_state == side:
-                    temp_moves = PIECE_MOVE_FUNCTIONS[piece_type]((x, y), board, side)
+                    if piece_type == 'p':
+                        temp_moves, temp_passant_moves = pawn_move((x, y), board, side, double_pawn)
+                        passant_moves.extend(temp_passant_moves)
+                    else:
+                        temp_moves = PIECE_MOVE_FUNCTIONS[piece_type]((x, y), board, side)
                     if temp_moves:
                         moves[(x, y)] = temp_moves
                     attacks = PIECE_ATTACK_FUNCTIONS[piece_type]((x, y), board, side)
@@ -218,7 +324,7 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
                     if attacks:
                         for attack in attacks:
                             enemy_attacks.setdefault(attack, []).append((x, y))
-                    if find_type((x, y), board) == 'k':
+                    if piece_type == 'k':
                         enemy_king = (x, y)
 
     if simulation_level == MAX_SIMULATION_LEVEL:
@@ -228,11 +334,11 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
             piece_type = find_type(piece, board)
             piece_points = PIECE_POINTS[piece_type]
             for move in moves[piece]:
-                _, deep_exchange_points = analyze_exchanges(move, piece, piece_type, board, ally_attacks, enemy_attacks,
-                                                            piece_points)
-                all_deep_exchange_points.append(deep_exchange_points)
+                all_deep_exchange_points.append(
+                    analyze_exchanges(move, piece, piece_type, piece_points, board, ally_attacks, enemy_attacks, passant_moves)[1]
+                )
         # print("MAX:", all_deep_exchange_points, max(all_deep_exchange_points))
-        return current_points + max(all_deep_exchange_points) * side * base_side
+        return (current_points + max(all_deep_exchange_points)) * -1
 
     else:
         final_moves = {}
@@ -243,15 +349,14 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
             piece_type = find_type(piece, board)
             piece_points = PIECE_POINTS[piece_type]
             for move in moves[piece]:
-                exchange_points, deep_exchange_points = analyze_exchanges(move, piece, piece_type, board, ally_attacks,
-                                                                          enemy_attacks,
-                                                                          piece_points)
+                exchange_points, deep_exchange_points = analyze_exchanges(move, piece, piece_type, piece_points, board,
+                                                                          ally_attacks, enemy_attacks, passant_moves)
                 # print(piece, move, exchange_points, deep_exchange_points, end=' ')
-                if side != base_side and exchange_points == PIECE_POINTS['k']:
-                    return current_points - PIECE_POINTS['k']
-                all_exchange_points[move] = exchange_points
-                if deep_exchange_points >= 0:
+                if exchange_points == PIECE_POINTS['k']:
+                    return (current_points + PIECE_POINTS['k']) * -1
+                elif deep_exchange_points >= 0:
                     final_moves.setdefault(piece, []).append(move)
+                    all_exchange_points[move] = exchange_points
         # print()
 
         if simulation_level > 1:
@@ -260,16 +365,24 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
             for piece in final_moves:
                 for move in final_moves[piece]:
                     # print(piece, move, end=' ')
-                    final_points.append(play(apply_move(move, piece, board, side), base_side, side * -1,
-                                             current_points + all_exchange_points[move] * base_side * side,
-                                             simulation_level + 1))
-            if side * base_side == -1:
-                return min(final_points)
-            else:
-                return max(final_points)
+                    # noinspection PyTypeChecker
+                    final_points.append(
+                        play(process_move(board, piece, move, side, move in passant_moves), side * -1, update_double_pawn(piece, move, find_type(piece, board)),
+                             (current_points + all_exchange_points[move]) * -1, simulation_level + 1)
+                    )
+            return max(final_points) * -1
 
         else:
-            current_points = analyze_board(board, base_side)
+
+            # move_number = 0
+            # for piece in final_moves:
+            #     move_number += len(final_moves[piece])
+            # for number, level in MAX_SIMULATION_LEVELS:
+            #     if move_number >= number:
+            #         max_level = level
+            #         break
+            # print(move_number, max_level)
+            current_points = analyze_board(board, side)
             # print(current_points)
             final_points = {}
 
@@ -277,8 +390,9 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
                 for move in final_moves[piece]:
                     # print('PIECE:', piece, move, 'move,',
                     #   all_exchange_points[move] * base_side * side, "exchange points")
-                    temp_points = play(apply_move(move, piece, board, side), base_side, side * -1,
-                                       current_points + all_exchange_points[move] * base_side * side,
+                    # noinspection PyTypeChecker
+                    temp_points = play(process_move(board, piece, move, side, move in passant_moves), side * -1,
+                                       update_double_pawn(piece, move, find_type(piece, board)), (current_points + all_exchange_points[move]) * -1,
                                        simulation_level + 1)
                     # print(temp_points, "final points")
                     final_points.setdefault(temp_points, {}).setdefault(piece, []).append(move)
@@ -287,13 +401,20 @@ def play(board, base_side, side, current_points=None, simulation_level=1):
             max_final_points = final_points[max(final_points)]
             if len(max_final_points.values()) == 1:
                 piece, move = list(max_final_points.items())[0]
-                return apply_move(move[0], piece, board, side)
-            end_points = {}
-            for piece in max_final_points:
-                piece_type = find_type(piece, board)
-                distance_to_king = distance(piece, enemy_king)
-                for move in max_final_points[piece]:
-                    end_points[analyze_movement(move, piece, piece_type, enemy_king, distance_to_king)] = (piece, move)
-            # print(end_points)
-            piece, move = end_points[max(end_points)]
-            return apply_move(move, piece, board, side)
+                move = move[0]
+            else:
+                castle_directions = find_castle_directions(board, side, enemy_attacks)
+                if castle_directions:
+                    return castle(board, side, castle_directions[0])
+                end_points = {}
+                for piece in max_final_points:
+                    piece_type = find_type(piece, board)
+                    distance_to_king = distance(piece, enemy_king)
+                    for move in max_final_points[piece]:
+                        end_points[analyze_movement(move, piece, piece_type, enemy_king, distance_to_king)] = (
+                            piece, move)
+                        # print(end_points)
+                piece, move = end_points[max(end_points)]
+
+            update_constants(board, side, piece)
+            return process_move(board, piece, move, side, move in passant_moves), update_double_pawn(piece, move, find_type(piece, board))
